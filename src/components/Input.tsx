32624d4e209cc0d881aa46392f6332fcdbd90a9b
icaroperetti <icaroperetti50@gmail.com>
2024-12-31 13:20:09 -0300

    refactor: format lint

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,39 +1,36 @@
 import { ComponentProps } from 'react'
-import {twMerge} from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge'
 
-interface PrefixProps extends ComponentProps<'div'> {}
+type PrefixProps = ComponentProps<'div'>
 
-export function Prefix(props:PrefixProps) {
-  return (
-    <div {...props}></div>
-  )
+export function Prefix(props: PrefixProps) {
+  return <div {...props}></div>
 }
 
-interface ControlProps extends ComponentProps<'input'> {}
+type ControlProps = ComponentProps<'input'>
 
-export function Control(props:ControlProps) {
+export function Control(props: ControlProps) {
   return (
     <input
-        className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:placeholder-zinc-400 dark:text-zinc-400"
-        placeholder="Search"
-        {...props}
-      />
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:text-zinc-400 dark:placeholder-zinc-400"
+      placeholder="Search"
+      {...props}
+    />
   )
 }
 
-interface InputRootProps extends ComponentProps<'div'>{}
+type InputRootProps = ComponentProps<'div'>
 
- export function Root(props: InputRootProps) {
+export function Root(props: InputRootProps) {
   return (
-    <div className={twMerge('flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ',
-      'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
-      'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-whitin:border-violet-500 dark:focus-within:ring-violet-500/20',
-      props.className
-    )}
-     {...props}>
-
-    </div>
+    <div
+      className={twMerge(
+        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm',
+        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'dark:focus-whitin:border-violet-500 dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:ring-violet-500/20',
+        props.className,
+      )}
+      {...props}
+    ></div>
   )
 }
-
-
