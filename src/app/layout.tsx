04503cc7f1fb23306a1abc595a89bc7809f693c8
icaroperetti <icaroperetti50@gmail.com>
2024-12-31 00:42:40 -0300

    feat: collapse sidebar on mobile screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} antialiased`}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-8 pb-12 pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+            {children}
+          </main>
         </div>
       </body>
     </html>
