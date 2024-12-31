be2b833bbb9072536968297b7df7cd0b8a1a036d
icaroperetti <icaroperetti50@gmail.com>
2024-12-31 11:33:00 -0300

    feat: dark theme form

--- src/components/Form/FileInput/FileItem.tsx
+++ src/components/Form/FileInput/FileItem.tsx
@@ -1,58 +1,67 @@
 import { Button } from '@/components/Button'
 import { formatBytes } from '@/utils/format-bytes'
 import { CheckCircle2, Trash2, UploadCloud } from 'lucide-react'
-import { tv,VariantProps} from 'tailwind-variants'
-
-interface  FileItemProps extends VariantProps<typeof fileItem> {
-  name: string
-  size: number
-}
+import { tv, VariantProps } from 'tailwind-variants'
 
 const fileItem = tv({
-  slots : {
-    container:'group flex items-start gap-4 rounded-lg border border-zinc-200 p-4',
-    icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600',
-    deleteButton:''
+  slots: {
+    container:
+      'group flex items-start gap-4 rounded-lg border border-zinc-200 p-4',
+    icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-500 dark:border-zinc-700 dark:bg-zin-800 dark:text-zinc-700',
+    deleteButton: '',
   },
+
   variants: {
     state: {
       progress: {
-        container: 'bg-zinc-50',
+        container: 'dark:border-zinc-700',
       },
       complete: {
-        container:'border-violet-500'
+        container: 'border-violet-500 dark:border-violet-300/30 dark:border-violet-300/30',
       },
       error: {
-        container:'bg-error-25 border border-error-300',
-        icon:'bg-error-50 bg-error-100 text-error-600',
-        deleteButton:'text-error-600 hover:text-error-900'
+        container:
+          'bg-error-25 border-error-300 dark:bg-error-500/5 dark:border-error-500/30',
+        icon: 'border-error-50 bg-error-100 text-error-600 dark:bg-error-500/5 dark:border-error-500/30 dark:text-error-400',
+        deleteButton:
+          'text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300',
       },
     },
   },
+
   defaultVariants: {
     state: 'progress',
   },
 })
-export function FileItem({ name, size,state }: FileItemProps) {
-  const {container,icon,deleteButton} = fileItem({state})
+
+export interface FileItemProps extends VariantProps<typeof fileItem> {
+  name: string
+  size: number
+}
+
+export function FileItem({ name, size, state }: FileItemProps) {
+  const { container, icon, deleteButton } = fileItem({ state })
 
   return (
     <div className={container()}>
       <div className={icon()}>
-        <UploadCloud className="h-5 w-5" />
+        <UploadCloud className="h-4 w-4" />
       </div>
 
       {state === 'error' ? (
         <div className="flex flex-1 flex-col items-start gap-1">
           <div className="flex flex-col">
-            <span className="text-error-700 text-sm font-medium">
-              Upload faild, try again
+            <span className="text-sm font-medium text-error-700 dark:text-error-400">
+              Upload failed, please try again.
             </span>
-            <span className="text-error-600 text-sm">{name}</span>
+            <span className="text-sm text-error-600 dark:text-error-500">
+              {name}
+            </span>
           </div>
+
           <button
             type="button"
-            className="text-error-700 hover:text-error-900 text-sm font-semibold"
+            className="text-sm font-semibold text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300"
           >
             Try again
           </button>
@@ -60,19 +69,25 @@ export function FileItem({ name, size,state }: FileItemProps) {
       ) : (
         <div className="flex flex-1 flex-col items-start gap-1">
           <div className="flex flex-col">
-            <span className="text-sm font-medium text-zinc-700">{name}</span>
-            <span className="text-sm text-zinc-500">{formatBytes(size)}</span>
+            <span className="text-sm font-medium text-zinc-700 dark:text-zinc-100">
+              {name}
+            </span>
+            <span className="text-sm text-zinc-500 dark:text-zinc-400">
+              {formatBytes(size)}
+            </span>
           </div>
 
           <div className="flex w-full items-center gap-3">
-            <div className="h-2 flex-1 rounded-full bg-violet-100">
+            <div className="h-2 flex-1 rounded-full bg-zinc-100 dark:bg-zinc-600">
               <div
-                className="h-2 rounded-full bg-violet-600"
-                style={{ width: state === 'complete' ? '100%' : '80%' }}
-              ></div>
+                className="h-2 rounded-full bg-violet-600 dark:bg-violet-400"
+                style={{
+                  width: state === 'complete' ? '100%' : '80%',
+                }}
+              />
             </div>
-            <span className="text-sm font-medium text-zinc-700">
-              {state === 'complete' ? '100%' : 'Uploading...'}
+            <span className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
+              {state === 'complete' ? '100%' : '80%'}
             </span>
           </div>
         </div>
@@ -82,7 +97,7 @@ export function FileItem({ name, size,state }: FileItemProps) {
         <CheckCircle2 className="h-5 w-5 fill-violet-600 text-white" />
       ) : (
         <Button type="button" variant="ghost" className={deleteButton()}>
-          <Trash2 className="h-5 w-5 " />
+          <Trash2 className="h-5 w-5" />
         </Button>
       )}
     </div>
